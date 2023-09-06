9ab475d45adf7067121eeaff7decf70c81fa67e4
David Herrera <david@alley.com>
2023-09-06 01:39:35 -0400

    WIP

--- src/alley/wp/posts/class-excluded-queries.php
+++ src/alley/wp/posts/class-excluded-queries.php
@@ -1,28 +1,28 @@
 <?php
 /**
- * Deduplicated_Post_Queries class file
+ * Excluded_Queries class file
  *
  * @package wp-type-extensions
  */
 
-namespace Alley\WP;
+namespace Alley\WP\Posts;
 
+use Alley\WP\Post_IDs_Query;
+use Alley\WP\Types\Post_IDs;
 use Alley\WP\Types\Post_Queries;
 use Alley\WP\Types\Post_Query;
 
 /**
- * Queries implementation that excludes posts marked as already used.
  */
-final class Deduplicated_Post_Queries implements Post_Queries {
+final class Excluded_Queries implements Post_Queries {
 	/**
 	 * Set up.
 	 *
-	 * @param Used_Post_IDs $used_post_ids  Used post IDs.
-	 * @param int           $posts_per_page Default posts per page.
-	 * @param Post_Queries  $origin         Post_Queries object.
+	 * @param Post_IDs     $exclude Excluded post IDs.
+	 * @param Post_Queries $origin  Post_Queries object.
 	 */
 	public function __construct(
-		private readonly Used_Post_IDs $used_post_ids,
+		private readonly Post_IDs $exclude,
 		private readonly int $posts_per_page,
 		private readonly Post_Queries $origin,
 	) {}
@@ -34,7 +34,7 @@ final class Deduplicated_Post_Queries implements Post_Queries {
 	 * @return Post_Query
 	 */
 	public function post_query_for_args( array $args ): Post_Query {
-		$used_post_ids     = $this->used_post_ids->post_ids();
+		$used_post_ids     = $this->exclude->post_ids();
 		$expected_per_page = $this->posts_per_page;
 
 		if ( isset( $args['posts_per_page'] ) && is_numeric( $args['posts_per_page'] ) ) {
@@ -45,14 +45,11 @@ final class Deduplicated_Post_Queries implements Post_Queries {
 		$args['posts_per_page'] = $expected_per_page + \count( $used_post_ids );
 		$overfetched_query      = $this->origin->post_query_for_args( $args );
 
-		// Remove the posts we've already used from the overfetched query.
+		// Remove the excluded from the overfetched query.
 		$unused_post_ids = array_diff( $overfetched_query->post_ids(), $used_post_ids );
 
 		// Slice the number of posts we expect to return from the overfetched query.
 		$per_page_post_ids = \array_slice( $unused_post_ids, 0, $expected_per_page );
-
-		// Record the new set of posts as used.
-		$this->used_post_ids->record( $per_page_post_ids );
 
 		return new Post_IDs_Query( $per_page_post_ids );
 	}
