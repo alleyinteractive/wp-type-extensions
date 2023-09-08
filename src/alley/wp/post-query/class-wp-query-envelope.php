54c4e7f631b12d3eaaeff0cf3e19824634d05a12
David Herrera <david@alley.com>
2023-09-08 01:36:20 -0400

    Split private static method to own class

--- src/alley/wp/post-query/class-wp-query-envelope.php
+++ src/alley/wp/post-query/class-wp-query-envelope.php
@@ -7,6 +7,7 @@
 
 namespace Alley\WP\Post_Query;
 
+use Alley\WP\Post_IDs\WP_Query_Post_IDs;
 use Alley\WP\Types\Post_Query;
 use WP_Post;
 use WP_Query;
@@ -51,53 +52,8 @@ final class WP_Query_Envelope implements Post_Query {
 	 * @return int[]
 	 */
 	public function post_ids(): array {
-		return self::to_post_ids( $this->query_object() );
-	}
+		$ids = new WP_Query_Post_IDs( $this->query_object() );
 
-	/**
-	 * Get the post IDs from a set of post objects, IDs, ID-parent objects, or a \WP_Query.
-	 *
-	 * @param array<WP_Post|int|object>|WP_Query $values Post-like objects or IDs, or a query object.
-	 * @return int[] Post IDs, if any.
-	 */
-	private static function to_post_ids( $values ): array {
-		$ids = [];
-
-		if ( $values instanceof WP_Query ) {
-			$ids = self::to_post_ids( (array) $values->posts );
-		} elseif ( \is_array( $values ) ) {
-			$ids = array_map( [ self::class, 'to_post_id' ], $values );
-		}
-
-		if ( $ids ) {
-			$ids = array_map( 'intval', $ids );
-			$ids = array_filter( $ids );
-		}
-
-		return $ids;
-	}
-
-	/**
-	 * Get the ID from a post object, ID, or ID-parent object.
-	 *
-	 * @param WP_Post|int|object $value Post-like object or ID.
-	 * @return int|null Post ID, or null on failure.
-	 */
-	private static function to_post_id( $value ): ?int {
-		if ( $value instanceof WP_Post ) {
-			return (int) $value->ID;
-		}
-
-		// fields => 'ids'.
-		if ( is_numeric( $value ) ) {
-			return (int) $value;
-		}
-
-		// fields => 'id=>parent'.
-		if ( ( $value instanceof \stdClass ) && isset( $value->ID ) ) {
-			return (int) $value->ID;
-		}
-
-		return null;
+		return $ids->post_ids();
 	}
 }
