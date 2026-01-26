da493f94f74fdfc7884916fd8a8b9e06d477d643
Sean Fisher <srtfisher@gmail.com>
2026-01-26 10:59:42 -0500

    Drop phpstan ignore

--- src/blocks/class-named-block.php
+++ src/blocks/class-named-block.php
@@ -58,6 +58,6 @@ final class Named_Block implements Single_Block {
 	 * @return string
 	 */
 	public function serialized_blocks(): string {
-		return serialize_block( $this->parsed_block() ); // @phpstan-ignore-line argument.type
+		return serialize_block( $this->parsed_block() );
 	}
 }
